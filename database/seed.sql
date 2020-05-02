cca203a96fd742b3e0fb4d28c7f350192466cbbe
Dustin Irving <dustin.irving@gmail.com>
2020-05-02 16:48:39 -0400

    Renamed schema and added seed files

--- database/seed.sql
+++ database/seed.sql
@@ -1,30 +1,3 @@
-DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE DATABASE employee_trackerDB;
-USE employee_trackerDB;
-
-CREATE TABLE employee (
-	id INTEGER AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    PRIMARY KEY (id)
-);
-    
-CREATE TABLE role (
-	id INTEGER AUTO_INCREMENT NOT NULL,
-	title VARCHAR(30) NOT NULL,
-	salary DECIMAL NOT NULL,
-	department_id INTEGER NOT NULL,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE department (
-	id INTEGER AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-);
-
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Tate', 'Bell', 1, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
@@ -66,10 +39,3 @@ INSERT INTO department (name)
 VALUES ('Finance');
 INSERT INTO department (name)
 VALUES ('Legal');
-
-
-
-
-
-
-        
